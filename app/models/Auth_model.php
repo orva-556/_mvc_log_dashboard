571ea3db1bc2f06df731a2751f56c545fabbb3c8
orva-556 <adiagum4.1@gmail.com>
2023-07-04 15:36:23 +0800

    ov

--- app/models/Auth_model.php
+++ app/models/Auth_model.php
@@ -17,13 +17,14 @@ class Auth_model
         if (isset($_POST['submit'])) {
             if ($_POST['username'] == $username && $_POST['password'] == $password) {
                 $_SESSION['username'] = $username;
-                // local
-                //header("Location: http://localhost/orva/_orva-local/log_dashboard");
-                // server
                 header("Location: " . BASEURL);
             } else {
-                header("location: " . BASEURL . "/auth");
-                // echo '<script>alert("Wrong Username or Password, check again!!!")</script>';
+                echo "<script>
+                alert('Wrong Username or Password, check again!!!');
+                window.location.href='" . BASEURL . "/auth';
+                </script>";
+                exit;
+                // header("location: " . BASEURL . "/auth");
             }
         }
     }
